fa09305d43895dc50a7063439f08e529b6ec4bd4
rickyle88 <bsbnguyen@gmail.com>
2019-04-01 06:24:27 -0700

    Update

--- Section07_EdgeLegerProject/js/main.js
+++ Section07_EdgeLegerProject/js/main.js
@@ -1,2 +1,19 @@
 // For Google Maps
 // jQuery
+
+// var map;
+function initMap() {
+
+    //Your location
+    const loc = {lat: 33.755131, lng: -117.981018};
+
+    // Centered map on location
+    map = new google.maps.Map(document.querySelector(".map"), {
+        // center: { lat: -34.397, lng: 150.644 },
+        center: loc,
+        zoom: 8
+    });
+
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({position: loc, map: map});
+}
